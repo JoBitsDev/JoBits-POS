8f1727ac8a530c2366bc4390ad98fe0b6ba42db7
Jorge <Jorge@localhost>
2018-09-27 18:52:00 -0400

    configurado para conectarse directamente a un host remoto

--- src/restManager/main/Main.java
+++ src/restManager/main/Main.java
@@ -21,7 +21,7 @@ public class Main {
     public static void main(String[] args) {
         
         
-        staticContent.init(R.RESOURCE_BUNDLE.getString("unidad_persistencia_remoto"));
+        staticContent.init(R.RESOURCE_BUNDLE.getString("unidad_persistencia_remota"));
         boolean newDatabase = checkIfDatabaseIsNew();
 
         if (newDatabase) {
