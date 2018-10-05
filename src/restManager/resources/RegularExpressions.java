710d2874dea8c986ad76027fafd9003716a76d30
Jorge <Jorge@localhost>
2018-10-04 22:25:28 -0400

    class created for storing the regular expressions used in the software

---
+++ src/restManager/resources/RegularExpressions.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package restManager.resources;
+
+/**
+ * FirstDream
+ * @author Jorge
+ * 
+ */
+public class RegularExpressions {
+
+    public static final String ONLY_WORDS_SEPARATED_WITH_SPACES = "[A-Z][[a-zA-Z]* X*[\\s]]*";
+    
+}
