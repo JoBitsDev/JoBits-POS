f8f2269195f17bcf052ea50c038e85a3dad27b0a
Jorge <Jorge@localhost>
2018-10-13 12:54:18 -0400

    implemented the remove instance method

--- src/restManager/controller/almacen/AlmacenTransaccionListController.java
+++ src/restManager/controller/almacen/AlmacenTransaccionListController.java
@@ -25,19 +25,19 @@ import restManager.persistencia.models.TransaccionDAO;
  *
  */
 public class AlmacenTransaccionListController extends AbstractController<Transaccion> {
-
+    
     AlmacenTransaccionsListView view;
     private Almacen a;
-
+    
     public AlmacenTransaccionListController() {
         super(new TransaccionDAO());
     }
-
+    
     public AlmacenTransaccionListController(JDialog parent) {
         this();
         constructView(parent);
     }
-
+    
     @Override
     public void createInstance() {
         Transaccion f = new Transaccion();
@@ -46,13 +46,19 @@ public class AlmacenTransaccionListController extends AbstractController<Transac
         f.setValorMerma(Float.parseFloat("0"));
         f.setAlmacencodAlmacen(a);
         f.setInsumoTransaccionList(new ArrayList<>());
-
+        
         AlmacenTransaccionManageController newTransaccion = new AlmacenTransaccionManageController(view, f);
         newTransaccion = null;
         items = null;
         view.updateView(getItems());
     }
-
+    
+    @Override
+    public void deleteInstance(Transaccion selected) {
+        super.deleteInstance(selected); //To change body of generated methods, choose Tools | Templates.
+        view.updateView(getItems());
+    }
+    
     @Override
     public void editInstance(Transaccion selected) {
         AlmacenTransaccionManageController newTransaccion = new AlmacenTransaccionManageController(view, selected);
@@ -60,12 +66,12 @@ public class AlmacenTransaccionListController extends AbstractController<Transac
         items = null;
         view.updateView(getItems());
     }
-
+    
     @Override
     public void constructView(Window parent) {
         view = new AlmacenTransaccionsListView(this, (Dialog) parent, true);
         view.updateView(getItems());
         view.setVisible(true);
     }
-
+    
 }
