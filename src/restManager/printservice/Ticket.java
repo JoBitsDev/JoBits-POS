0e8479f277d66043b18529b902fcbff5066643c0
Jorge <Jorge@localhost>
2018-11-02 10:59:04 -0400

    created some methods for compatibility while usign 80mm thermal printer with 58mm paper

--- src/restManager/printservice/Ticket.java
+++ src/restManager/printservice/Ticket.java
@@ -5,17 +5,21 @@
  */
 package restManager.printservice;
 
-
 public class Ticket {
+
     String commandSet = "";
-    final static int PAPER_LENGHT = 32; 
-    final static char LINE_CHAR= '*';
+
+    /**
+     * the paper lenght is 32 for 58mm and 48 for 80mm
+     */
+    final static int PAPER_LENGHT = 32;
+    final static char LINE_CHAR = '*';
+
+    private AlignmentState posState = AlignmentState.LEFT;
 
     public Ticket() {
     }
 
-    
-    
     public String initialize() {
         final byte[] Init = {27, 64};
         commandSet += new String(Init);
@@ -29,127 +33,134 @@ public class Ticket {
         final byte[] ChooseFontC = {27, 77, 48};
         final byte[] ChooseFontD = {27, 77, 49};
 
-        switch(Options) {
+        switch (Options) {
             case 1:
-            s = new String(ChooseFontA);
-            break;
+                s = new String(ChooseFontA);
+                break;
 
             case 2:
-            s = new String(ChooseFontB);
-            break;
+                s = new String(ChooseFontB);
+                break;
 
             case 3:
-            s = new String(ChooseFontC);
-            break;
+                s = new String(ChooseFontC);
+                break;
 
             case 4:
-            s = new String(ChooseFontD);
-            break;
+                s = new String(ChooseFontD);
+                break;
 
             default:
-            s = new String(ChooseFontB);
+                s = new String(ChooseFontB);
         }
         commandSet += s;
         return new String(s);
     }
 
     public String feedBack(byte lines) {
-        final byte[] Feed = {27,101,lines};
+        final byte[] Feed = {27, 101, lines};
         String s = new String(Feed);
         commandSet += s;
         return s;
     }
 
     public String feed(byte lines) {
-        final byte[] Feed = {27,100,lines};
+        final byte[] Feed = {27, 100, lines};
         String s = new String(Feed);
         commandSet += s;
         return s;
     }
 
     public String alignLeft() {
-        final byte[] AlignLeft = {27, 97,48};
+        posState = AlignmentState.LEFT;
+        final byte[] AlignLeft = {27, 97, 48};
         String s = new String(AlignLeft);
         commandSet += s;
         return s;
     }
 
     public String alignCenter() {
-        final byte[] AlignCenter = {27, 97,49};
-        String s = new String(AlignCenter);
-        commandSet += s;
-        return s;
+        posState = AlignmentState.CENTER;
+//        final byte[] AlignCenter = {27, 97, 49};
+//        String s = new String(AlignCenter);
+//        commandSet += s;
+        return "";
     }
 
     public String alignRight() {
-        final byte[] AlignRight = {27, 97,50};
-        String s = new String(AlignRight);
+        posState = AlignmentState.RIGHT;
+//        final byte[] AlignRight = {27, 97, 50};
+//        String s = new String(AlignRight);
+//        commandSet += s;
+        return "";
+    }
+
+    public String newLine() {
+        final byte[] LF = {10};
+        String s = new String(LF);
         commandSet += s;
         return s;
     }
 
-    public String newLine() {
-        final  byte[] LF = {10};
-        String s = new String(LF);
-        commandSet += s;
-        return s;
-   }
-
-   public String reverseColorMode(boolean enabled) {
+    public String reverseColorMode(boolean enabled) {
         final byte[] ReverseModeColorOn = {29, 66, 1};
         final byte[] ReverseModeColorOff = {29, 66, 0};
 
         String s = "";
-        if(enabled)
+        if (enabled) {
             s = new String(ReverseModeColorOn);
-        else
+        } else {
             s = new String(ReverseModeColorOff);
+        }
 
         commandSet += s;
         return s;
-    } 
+    }
 
     public String doubleStrik(boolean enabled) {
         final byte[] DoubleStrikeModeOn = {27, 71, 1};
         final byte[] DoubleStrikeModeOff = {27, 71, 0};
 
-        String s="";
-        if(enabled)
+        String s = "";
+        if (enabled) {
             s = new String(DoubleStrikeModeOn);
-        else
+        } else {
             s = new String(DoubleStrikeModeOff);
+        }
 
         commandSet += s;
         return s;
-    } 
+    }
 
     public String doubleHeight(boolean enabled) {
         final byte[] DoubleHeight = {27, 33, 17};
-        final byte[] UnDoubleHeight={27, 33, 0};
+        final byte[] UnDoubleHeight = {27, 33, 0};
 
         String s = "";
-        if(enabled)
+        if (enabled) {
             s = new String(DoubleHeight);
-        else
+        } else {
             s = new String(UnDoubleHeight);
+        }
 
         commandSet += s;
         return s;
     }
 
     public String emphasized(boolean enabled) {
-        final byte[] EmphasizedOff={27 ,0};
-        final byte[] EmphasizedOn={27 ,1};
+        final byte[] EmphasizedOff = {27, 0};
+        final byte[] EmphasizedOn = {27, 1};
 
-        String s="";
-        if(enabled)
+        String s = "";
+        if (enabled) {
             s = new String(EmphasizedOn);
-        else
+        } else {
             s = new String(EmphasizedOff);
+        }
 
         commandSet += s;
         return s;
-    } 
+    }
 
     public String underLine(int Options) {
         final byte[] UnderLine2Dot = {27, 45, 50};
@@ -157,17 +168,17 @@ public class Ticket {
         final byte[] NoUnderLine = {27, 45, 48};
 
         String s = "";
-        switch(Options) {
+        switch (Options) {
             case 0:
-            s = new String(NoUnderLine);
-            break;
+                s = new String(NoUnderLine);
+                break;
 
             case 1:
-            s = new String(UnderLine1Dot);
-            break;
+                s = new String(UnderLine1Dot);
+                break;
 
             default:
-            s = new String(UnderLine2Dot);
+                s = new String(UnderLine2Dot);
         }
         commandSet += s;
         return new String(s);
@@ -178,17 +189,17 @@ public class Ticket {
         final byte[] ColorBlack = {27, 114, 48};
 
         String s = "";
-        switch(Options) {
+        switch (Options) {
             case 0:
-            s = new String(ColorBlack);
-            break;
+                s = new String(ColorBlack);
+                break;
 
             case 1:
-            s = new String(ColorRed);
-            break;
+                s = new String(ColorRed);
+                break;
 
             default:
-            s = new String(ColorBlack);
+                s = new String(ColorBlack);
         }
         commandSet += s;
         return s;
@@ -199,16 +210,17 @@ public class Ticket {
 
         String s = new String(FeedAndCut);
 
-        final byte[] DrawerKick={27,70,0,60,120};   
+        final byte[] DrawerKick = {27, 70, 0, 60, 120};
         s += new String(DrawerKick);
 
-        commandSet+=s;
+        commandSet += s;
         return s;
     }
 
     public String addLineSeperator() {
+        alignLeft();
         String lineSpace = "";
-        while(lineSpace.length()<PAPER_LENGHT){
+        while (lineSpace.length() < PAPER_LENGHT) {
             lineSpace += LINE_CHAR;
         }
         commandSet += lineSpace;
@@ -220,10 +232,49 @@ public class Ticket {
     }
 
     public void setText(String s) {
-        commandSet+=s;
+        int sLenght = s.length();
+        do {
+            sLenght = s.length();
+            if (sLenght > PAPER_LENGHT) {
+                writeText(s.substring(0, PAPER_LENGHT));
+                s = s.substring(PAPER_LENGHT);
+                newLine();
+            } else {
+                writeText(s);
+            }
+        } while (sLenght > PAPER_LENGHT);
+
+    }
+
+    private void writeText(String s) {
+        switch (posState) {
+            case LEFT:
+                commandSet += s;
+                break;
+            case CENTER:
+                commandSet += (addBlankSpaces((PAPER_LENGHT - s.length()) / 2)
+                        + s
+                        + addBlankSpaces((PAPER_LENGHT - s.length()) / 2));
+                break;
+            case RIGHT:
+                commandSet += (addBlankSpaces(PAPER_LENGHT - s.length()) + s);
+                break;
+        }
     }
 
     public String finalCommandSet() {
         return commandSet;
+    }
+
+    private String addBlankSpaces(int amount) {
+        String ret = "";
+        for (int i = 0; i < amount; i++) {
+            ret += " ";
+        }
+        return ret;
+    }
+
+    private enum AlignmentState {
+        LEFT, RIGHT, CENTER
     }
 }
