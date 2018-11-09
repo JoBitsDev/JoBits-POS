96e073947260963e98928c80279f07f8bcb1693c
Jorge <Jorge@localhost>
2018-11-09 15:09:59 -0500

    added an exception that occurs in the view when validating

---
+++ src/restManager/exceptions/ValidatingException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restManager.exceptions;
+
+import javax.swing.JOptionPane;
+import restManager.resources.R;
+
+/**
+ * FirstDream
+ *
+ * @author Jorge
+ *
+ */
+public class ValidatingException extends RuntimeException {
+
+    public ValidatingException() {
+        JOptionPane.showMessageDialog(null, R.RESOURCE_BUNDLE.getString("exception_validating"),
+                R.RESOURCE_BUNDLE.getString("label_error"), JOptionPane.ERROR_MESSAGE);
+    }
+
+    public ValidatingException(String message) {
+        super(message);
+        JOptionPane.showMessageDialog(null, message,
+                R.RESOURCE_BUNDLE.getString("label_error"), JOptionPane.ERROR_MESSAGE);
+    }
+
+}
