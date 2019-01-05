2a245d646bcc4240b9ca998f7579c85f28d99ba0
Jorge <Jorge@localhost>
2019-01-05 01:42:47 -0500

    new model for carta persistence table

---
+++ src/restManager/persistencia/models/CartaDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package restManager.persistencia.models;
+
+import restManager.persistencia.Carta;
+
+/**
+ * FirstDream
+ * @author Jorge
+ * 
+ */
+public class CartaDAO extends AbstractModel<Carta>{
+
+    public CartaDAO() {
+        super(Carta.class);
+    }
+    
+}
