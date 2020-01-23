540a2fbd73e6633f092001f9e544cac54138de9d
Jorge <Jorge@localhost>
2020-01-23 08:38:39 -0500

    Creada la vista con los modelos para las tablas

---
+++ src/restManager/persistencia/models/utils/InsumoPedidoModel.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restManager.persistencia.models.utils;
+
+import restManager.persistencia.Insumo;
+
+/**
+ * FirstDream
+ *
+ * @author Jorge
+ *
+ */
+public class InsumoPedidoModel {
+
+    private final Insumo insumo;
+    private final float cantidad;
+
+    public InsumoPedidoModel(Insumo insumo, float cantidad) {
+        this.insumo = insumo;
+        this.cantidad = cantidad;
+    }
+
+    public Insumo getInsumo() {
+        return insumo;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+}
