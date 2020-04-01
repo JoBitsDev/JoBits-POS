20d6254b4f37b17cd2da1825d61bc81aab2824dc
Jorge <Jorge@localhost>
2020-04-01 15:59:04 -0400

    Movido de paquetes

--- src/restManager/persistencia/volatil/InsumoPedidoModel.java
+++ src/restManager/persistencia/volatil/InsumoPedidoModel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package restManager.persistencia.models.utils;
+package restManager.persistencia.volatil;
 
 import restManager.persistencia.Insumo;
 
