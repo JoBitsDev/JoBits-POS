d13c9467a8ccf68e4702240c6322906f19b4479e
Jorge <Jorge@localhost>
2020-05-06 16:51:45 -0400

    Duplicado de capa de persistencia

---
+++ src/main/java/com/jobits/pos/adapters/repo/daos/NotificacionEnvioCocina.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jobits.pos.adapters.repo.daos;
+
+import com.jobits.pos.domain.models.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * FirstDream
+ * @author Jorge
+ * 
+ */
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "notificacionEnvioCocinaPK",scope = NotificacionEnvioCocina.class )
+@Entity
+@Table(name = "notificacion_envio_cocina")
+@NamedQueries({
+    @NamedQuery(name = "NotificacionEnvioCocina.findAll", query = "SELECT n FROM NotificacionEnvioCocina n"),
+    @NamedQuery(name = "NotificacionEnvioCocina.findByCocinacodCocina", query = "SELECT n FROM NotificacionEnvioCocina n WHERE n.notificacionEnvioCocinaPK.cocinacodCocina = :cocinacodCocina"),
+    @NamedQuery(name = "NotificacionEnvioCocina.findByProductovOrdenproductoVentapCod", query = "SELECT n FROM NotificacionEnvioCocina n WHERE n.notificacionEnvioCocinaPK.productovOrdenproductoVentapCod = :productovOrdenproductoVentapCod"),
+    @NamedQuery(name = "NotificacionEnvioCocina.findByProductovOrdenordencodOrden", query = "SELECT n FROM NotificacionEnvioCocina n WHERE n.notificacionEnvioCocinaPK.productovOrdenordencodOrden = :productovOrdenordencodOrden"),
+    @NamedQuery(name = "NotificacionEnvioCocina.findByCantidad", query = "SELECT n FROM NotificacionEnvioCocina n WHERE n.cantidad = :cantidad"),
+    @NamedQuery(name = "NotificacionEnvioCocina.findByHoraNotificacion", query = "SELECT n FROM NotificacionEnvioCocina n WHERE n.horaNotificacion = :horaNotificacion")})
+public class NotificacionEnvioCocina implements Serializable {
+
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "cantidad")
+    private Float cantidad;
+
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    protected NotificacionEnvioCocinaPK notificacionEnvioCocinaPK;
+    @Column(name = "hora_notificacion")
+    @Temporal(TemporalType.TIME)
+    private Date horaNotificacion;
+    @JoinColumn(name = "cocinacod_cocina", referencedColumnName = "cod_cocina", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Cocina cocina;
+    @JoinColumns({
+        @JoinColumn(name = "productov_ordenproducto_ventap_cod", referencedColumnName = "producto_ventap_cod", insertable = false, updatable = false),
+        @JoinColumn(name = "productov_ordenordencod_orden", referencedColumnName = "ordencod_orden", insertable = false, updatable = false)})
+    @ManyToOne(optional = false)
+    private ProductovOrden productovOrden;
+
+    @Column(name = "ip_dependiente")
+    private String ip_dependiente;
+    
+    public NotificacionEnvioCocina() {
+    }
+
+    public NotificacionEnvioCocina(NotificacionEnvioCocinaPK notificacionEnvioCocinaPK) {
+        this.notificacionEnvioCocinaPK = notificacionEnvioCocinaPK;
+    }
+
+    public NotificacionEnvioCocina(String cocinacodCocina, String productovOrdenproductoVentapCod, String productovOrdenordencodOrden) {
+        this.notificacionEnvioCocinaPK = new NotificacionEnvioCocinaPK(cocinacodCocina, productovOrdenproductoVentapCod, productovOrdenordencodOrden);
+    }
+
+    public NotificacionEnvioCocinaPK getNotificacionEnvioCocinaPK() {
+        return notificacionEnvioCocinaPK;
+    }
+
+    public void setNotificacionEnvioCocinaPK(NotificacionEnvioCocinaPK notificacionEnvioCocinaPK) {
+        this.notificacionEnvioCocinaPK = notificacionEnvioCocinaPK;
+    }
+
+
+    public Date getHoraNotificacion() {
+        return horaNotificacion;
+    }
+
+    public void setHoraNotificacion(Date horaNotificacion) {
+        this.horaNotificacion = horaNotificacion;
+    }
+
+    public Cocina getCocina() {
+        return cocina;
+    }
+
+    public void setCocina(Cocina cocina) {
+        this.cocina = cocina;
+    }
+
+    public ProductovOrden getProductovOrden() {
+        return productovOrden;
+    }
+
+    public void setProductovOrden(ProductovOrden productovOrden) {
+        this.productovOrden = productovOrden;
+    }
+
+    public String getIp_dependiente() {
+        return ip_dependiente;
+    }
+
+    public void setIp_dependiente(String ip_dependiente) {
+        this.ip_dependiente = ip_dependiente;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (notificacionEnvioCocinaPK != null ? notificacionEnvioCocinaPK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NotificacionEnvioCocina)) {
+            return false;
+        }
+        NotificacionEnvioCocina other = (NotificacionEnvioCocina) object;
+        if ((this.notificacionEnvioCocinaPK == null && other.notificacionEnvioCocinaPK != null) || (this.notificacionEnvioCocinaPK != null && !this.notificacionEnvioCocinaPK.equals(other.notificacionEnvioCocinaPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "restManager.persistencia.NotificacionEnvioCocina[ notificacionEnvioCocinaPK=" + notificacionEnvioCocinaPK + " ]";
+    }
+
+    public Float getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+}
---
+++ src/main/java/com/jobits/pos/adapters/repo/daos/Venta.java
@@ -0,0 +1,222 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.adapters.repo.daos;
+
+import com.jobits.pos.domain.models.*;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import com.jobits.pos.recursos.R;
+
+/**
+ * FirstDream
+ *
+ * @author Jorge
+ *
+ */
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "fecha",scope = Venta.class )
+@Entity
+@Table(name = "venta")
+@NamedQueries({
+    @NamedQuery(name = "Venta.findAll", query = "SELECT v FROM Venta v"),
+    @NamedQuery(name = "Venta.findByFecha", query = "SELECT v FROM Venta v WHERE v.fecha = :fecha"),
+    @NamedQuery(name = "Venta.findByVentaTotal", query = "SELECT v FROM Venta v WHERE v.ventaTotal = :ventaTotal"),
+    @NamedQuery(name = "Venta.findByVentagastosEninsumos", query = "SELECT v FROM Venta v WHERE v.ventagastosEninsumos = :ventagastosEninsumos"),
+    @NamedQuery(name = "Venta.findByHoraPico", query = "SELECT v FROM Venta v WHERE v.horaPico = :horaPico"),
+    @NamedQuery(name = "Venta.findByVentagastosPagotrabajadores", query = "SELECT v FROM Venta v WHERE v.ventagastosPagotrabajadores = :ventagastosPagotrabajadores"),
+    @NamedQuery(name = "Venta.findByVentagastosGastos", query = "SELECT v FROM Venta v WHERE v.ventagastosGastos = :ventagastosGastos"),
+    @NamedQuery(name = "Venta.findByVentagastosEninsumos", query = "SELECT v FROM Venta v WHERE v.ventagastosEninsumos = :ventagastosEninsumos")})
+public class Venta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    @Column(name = "ventapropina")
+    private Float ventapropina;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "venta")
+    private List<AsistenciaPersonal> asistenciaPersonalList;
+
+    @Column(name = "cambio_turno1")
+    private String cambioTurno1;
+    @Column(name = "cambio_turno2")
+    private String cambioTurno2;
+
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "venta_total")
+    private Double ventaTotal;
+    @Column(name = "ventagastos_eninsumos")
+    private Double ventagastosEninsumos;
+    @Column(name = "hora_pico")
+    private Integer horaPico;
+    @Column(name = "ventagastos_pagotrabajadores")
+    private Float ventagastosPagotrabajadores;
+    @Column(name = "ventagastos_gastos")
+    private Float ventagastosGastos;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "venta")
+    private List<GastoVenta> gastoVentaList;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ventafecha")
+    private List<Orden> ordenList;
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "fechaVenta")
+    private List<IpvVentaRegistro> ipvVentaRegistroList;
+
+    public Venta() {
+    }
+
+    public Venta(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getVentaTotal() {
+        return ventaTotal;
+    }
+
+    public void setVentaTotal(Double ventaTotal) {
+        this.ventaTotal = ventaTotal;
+    }
+
+    public Double getVentagastosEninsumos() {
+        return ventagastosEninsumos;
+    }
+
+    public void setVentagastosEninsumos(Double ventagastosEninsumos) {
+        this.ventagastosEninsumos = ventagastosEninsumos;
+    }
+
+    public Integer getHoraPico() {
+        return horaPico;
+    }
+
+    public List<Orden> getOrdenList() {
+        return ordenList;
+    }
+
+    public void setOrdenList(List<Orden> ordenList) {
+        this.ordenList = ordenList;
+    }
+
+    public void setHoraPico(Integer horaPico) {
+        this.horaPico = horaPico;
+    }
+
+    public Float getVentagastosPagotrabajadores() {
+        return ventagastosPagotrabajadores;
+    }
+
+    public void setVentagastosPagotrabajadores(Float ventagastosPagotrabajadores) {
+        this.ventagastosPagotrabajadores = ventagastosPagotrabajadores;
+    }
+
+    public Float getVentagastosGastos() {
+        return ventagastosGastos;
+    }
+
+    public void setVentagastosGastos(Float ventagastosGastos) {
+        this.ventagastosGastos = ventagastosGastos;
+    }
+
+    public List<GastoVenta> getGastoVentaList() {
+        return gastoVentaList;
+    }
+
+    public void setGastoVentaList(List<GastoVenta> gastoVentaList) {
+        this.gastoVentaList = gastoVentaList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (fecha != null ? fecha.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Venta)) {
+            return false;
+        }
+        Venta other = (Venta) object;
+        if ((this.fecha == null && other.fecha != null) || (this.fecha != null && !this.fecha.equals(other.fecha))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta del dia " + R.DATE_FORMAT.format(fecha);
+    }
+
+    public String getCambioTurno1() {
+        return cambioTurno1;
+    }
+
+    public void setCambioTurno1(String cambioTurno1) {
+        this.cambioTurno1 = cambioTurno1;
+    }
+
+    public String getCambioTurno2() {
+        return cambioTurno2;
+    }
+
+    public void setCambioTurno2(String cambioTurno2) {
+        this.cambioTurno2 = cambioTurno2;
+    }
+
+    public List<AsistenciaPersonal> getAsistenciaPersonalList() {
+        return asistenciaPersonalList;
+    }
+
+    public void setAsistenciaPersonalList(List<AsistenciaPersonal> asistenciaPersonalList) {
+        this.asistenciaPersonalList = asistenciaPersonalList;
+    }
+
+    public Float getVentapropina() {
+        return ventapropina;
+    }
+
+    public void setVentapropina(Float ventapropina) {
+        this.ventapropina = ventapropina;
+    }
+
+    public List<IpvVentaRegistro> getIpvVentaRegistroList() {
+        return ipvVentaRegistroList;
+    }
+
+    public void setIpvVentaRegistroList(List<IpvVentaRegistro> ipvVentaRegistroList) {
+        this.ipvVentaRegistroList = ipvVentaRegistroList;
+    }
+
+}
