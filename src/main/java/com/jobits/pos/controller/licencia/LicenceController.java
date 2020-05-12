4f6461e4aaf05097e50d6706abd1481607667811
Jorge <Jorge@localhost>
2020-05-12 12:24:40 -0400

    estandarizado strings

--- src/main/java/com/jobits/pos/controller/licencia/LicenceController.java
+++ src/main/java/com/jobits/pos/controller/licencia/LicenceController.java
@@ -5,18 +5,11 @@
  */
 package com.jobits.pos.controller.licencia;
 
-import com.jobits.pos.ui.OldAbstractView;
-import com.jobits.pos.ui.licencia.LicenceDialogView;
-import java.awt.Container;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import com.jobits.pos.controller.AbstractDialogController;
-import com.jobits.pos.domain.models.Configuracion;
-import com.jobits.pos.adapters.repo.ConfiguracionDAO;
-import com.jobits.pos.ui.OldView;
 
 /**
  * FirstDream
@@ -24,38 +17,31 @@ import com.jobits.pos.ui.OldView;
  * @author Jorge
  *
  */
-public class LicenceController extends AbstractDialogController<Configuracion> {
+public class LicenceController {
 
-    private final Licence licence;
-    private Container parent;
-    private Licence.TipoLicencia tipoLic;
-    private final String estadoLic;
+    public static final String LICENCIA_INVALIDA = "Licencia invalida";
+    public static final String ERROR_LECTURA_LICENCIA = "Error de lectura de la licencia";
+    public static final String LICENCIA_NO_ENCONTRADA = "Archivo de licencia no encontrado o ilegible";
+    public static final String ERROR_ESCRITURA = "Error de escritura";
 
+    private final Licence licence = Licence.getInstance();
+    private Licence.TipoLicencia tipoLic = Licence.TipoLicencia.APLICACION;
+    private String estadoLic = LICENCIA_INVALIDA;
+
+    public LicenceController() {
+    }
+    
     public LicenceController(Licence.TipoLicencia tipoLic) {
-        super(ConfiguracionDAO.getInstance());
-        licence = Licence.getInstance();
         this.tipoLic = tipoLic;
         estadoLic = getEstadoLicencia(this.tipoLic);
     }
 
-    public LicenceController(Container parent, Licence.TipoLicencia tipoLic) {
-       this(tipoLic);
-        this.parent = parent;
-        constructView(parent);
-    }
-
-    @Override
-    public void constructView(Container parent) {
-        setView(new LicenceDialogView(OldView.DialogType.DEFINED, this, (OldAbstractView) parent, true));
-        getView().updateView();
-        getView().setVisible(true);
-    }
 
     public String getEstadoLicencia(Licence.TipoLicencia tipo) {
         licence.reset();
         File f = new File(tipo.getPath());
         if (!f.exists() || !f.canRead()) {
-            return "Archivo de licencia no encontrado o ilegible";
+            return LICENCIA_NO_ENCONTRADA;
         }
         FileInputStream in;
         String key;
@@ -65,26 +51,26 @@ public class LicenceController extends AbstractDialogController<Configuracion> {
             in.read(b);
             key = new String(b);
         } catch (FileNotFoundException ex) {
-            return "Error de lectura de la licencia";
+            return ERROR_LECTURA_LICENCIA;
         } catch (IOException ex) {
-            return "Error de lectura de la licencia";
+            return ERROR_LECTURA_LICENCIA;
         }
         Licence.getInstance().setTipoLicencia(tipo);
         Licence.getInstance().setLicence(key.replaceAll("-", ""));
         if (licence.LICENCIA_VALIDA && licence.LICENCIA_ACTIVA) {
             return "Dias restantes " + licence.DIAS_RESTANTES;
         } else {
-            return "Licencia invalida";
+            return LICENCIA_INVALIDA;
         }
     }
 
     public void validateAndSafe(String key) {
         if (validateLicence(key)) {
-            safeLicence(key);
-            showSuccessDialog(getView(), "Licencia activa por " + licence.DIAS_RESTANTES + " Dias");
-            getView().dispose();
+            saveLicence(key);
+            // showSuccessDialog(getView(), "Licencia activa por " + licence.DIAS_RESTANTES + " Dia(s)");
+            //getView().dispose();
         } else {
-            showErrorDialog(getView(), "La licencia no es valida");
+//            showErrorDialog(getView(), LICENCIA_INVALIDA);
         }
     }
 
@@ -93,15 +79,15 @@ public class LicenceController extends AbstractDialogController<Configuracion> {
         return licence.LICENCIA_ACTIVA && licence.LICENCIA_VALIDA;
     }
 
-    private void safeLicence(String key) {
+    private void saveLicence(String key) {
         File f = new File(tipoLic.getPath());
         try (FileOutputStream out = new FileOutputStream(f)) {
             out.write(key.getBytes());
             out.flush();
         } catch (FileNotFoundException ex) {
-            showErrorDialog(getView(), "El archivo de licencia no se encontro");
+            // showErrorDialog(getView(), LICENCIA_NO_ENCONTRADA);
         } catch (IOException ex) {
-            showErrorDialog(getView(), "Error de escritura");
+            //showErrorDialog(getView(), ERROR_ESCRITURA);
         }
     }
 
