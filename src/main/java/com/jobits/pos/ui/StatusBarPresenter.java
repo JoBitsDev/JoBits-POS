cd99ec52807e9e6f9497216653a5368a4f349684
Jorge <Jorge@localhost>
2020-05-12 14:47:06 -0400

    status bar setted

---
+++ src/main/java/com/jobits/pos/ui/StatusBarPresenter.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.ui;
+
+import com.jobits.pos.controller.licencia.Licence;
+import com.jobits.pos.controller.licencia.LicenceController;
+import com.jobits.pos.main.Application;
+import com.jobits.pos.recursos.R;
+import com.jobits.pos.ui.presenters.AbstractViewAction;
+import com.jobits.pos.ui.presenters.AbstractViewPresenter;
+import java.util.Optional;
+
+/**
+ *
+ * JoBits
+ *
+ * @author Jorge
+ *
+ */
+public class StatusBarPresenter extends AbstractViewPresenter<StatusBarViewModel> {
+
+    private final LicenceController controller = Application.getInstance().getLicenceController();
+    public static final String ACTION_LICENCIA = "";
+    public static final String ACTION_REFRESH_BEAN = "Refresh";
+
+    public StatusBarPresenter() {
+        super(new StatusBarViewModel());
+        refreshBean();
+
+    }
+
+    private void onLicenciaButtonClick() {
+
+    }
+
+    private void refreshBean() {
+        getBean().setBoton_licencia_habilitado(true);
+        getBean().setEstado_licencia(controller.getEstadoLicencia(Licence.TipoLicencia.APLICACION));
+        if (Application.getInstance().getLoggedUser() != null) {
+            getBean().setUsuario_registrado(Application.getInstance().getLoggedUser().getUsuario());
+        }
+        getBean().setVersion_software(R.RELEASE_VERSION);
+    }
+
+    @Override
+    protected void registerOperations() {
+        registerOperation(new AbstractViewAction(ACTION_LICENCIA) {
+            @Override
+            public Optional doAction() {
+                onLicenciaButtonClick();
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_REFRESH_BEAN) {
+            @Override
+            public Optional doAction() {
+                refreshBean();
+                return Optional.empty();
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/jobits/pos/ui/StatusBarViewModel.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.ui;
+
+import com.jobits.pos.ui.viewmodel.AbstractViewModel;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ * JoBits
+ *
+ * @author Jorge
+ *
+ */
+public class StatusBarViewModel extends AbstractViewModel {
+
+    private String estado_licencia;
+
+    public static final String PROP_ESTADO_LICENCIA = "estado_licencia";
+
+    private String usuario_registrado;
+
+    public static final String PROP_USUARIO_REGISTRADO = "usuario_registrado";
+
+    private String version_software;
+
+    public static final String PROP_VERSION_SOFTWARE = "version_software";
+
+    private boolean boton_licencia_habilitado;
+
+    public static final String PROP_BOTON_LICENCIA_HABILITADO = "boton_licencia_habilitado";
+    
+
+    /**
+     * Get the value of boton_licencia_habilitado
+     *
+     * @return the value of boton_licencia_habilitado
+     */
+    public boolean isBoton_licencia_habilitado() {
+        return boton_licencia_habilitado;
+    }
+
+    /**
+     * Set the value of boton_licencia_habilitado
+     *
+     * @param boton_licencia_habilitado new value of boton_licencia_habilitado
+     */
+    public void setBoton_licencia_habilitado(boolean boton_licencia_habilitado) {
+        boolean oldBoton_licencia_habilitado = this.boton_licencia_habilitado;
+        this.boton_licencia_habilitado = boton_licencia_habilitado;
+        firePropertyChange(PROP_BOTON_LICENCIA_HABILITADO, oldBoton_licencia_habilitado, boton_licencia_habilitado,true);
+    }
+
+    /**
+     * Get the value of version_software
+     *
+     * @return the value of version_software
+     */
+    public String getVersion_software() {
+        return version_software;
+    }
+
+    /**
+     * Set the value of version_software
+     *
+     * @param version_software new value of version_software
+     */
+    public void setVersion_software(String version_software) {
+        String oldVersion_software = this.version_software;
+        this.version_software = version_software;
+        firePropertyChange(PROP_VERSION_SOFTWARE, oldVersion_software, version_software,false);
+    }
+
+    /**
+     * Get the value of usuario_registrado
+     *
+     * @return the value of usuario_registrado
+     */
+    public String getUsuario_registrado() {
+        return usuario_registrado;
+    }
+
+    /**
+     * Set the value of usuario_registrado
+     *
+     * @param usuario_registrado new value of usuario_registrado
+     */
+    public void setUsuario_registrado(String usuario_registrado) {
+        String oldUsuario_registrado = this.usuario_registrado;
+        this.usuario_registrado = usuario_registrado;
+        firePropertyChange(PROP_USUARIO_REGISTRADO, oldUsuario_registrado, usuario_registrado,false);
+    }
+
+    /**
+     * Get the value of estado_licencia
+     *
+     * @return the value of estado_licencia
+     */
+    public String getEstado_licencia() {
+        return estado_licencia;
+    }
+
+    /**
+     * Set the value of estado_licencia
+     *
+     * @param estado_licencia new value of estado_licencia
+     */
+    public void setEstado_licencia(String estado_licencia) {
+        String oldEstado_licencia = this.estado_licencia;
+        this.estado_licencia = estado_licencia;
+        firePropertyChange(PROP_ESTADO_LICENCIA, oldEstado_licencia, estado_licencia,false);
+    }
+
+}
