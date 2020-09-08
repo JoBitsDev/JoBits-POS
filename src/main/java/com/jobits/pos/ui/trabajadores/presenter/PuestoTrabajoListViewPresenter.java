3761c044a84f72304a74f23df0452a520ff6cbf0
Jorge <Jorge@localhost>
2020-09-08 17:51:34 -0400

    Ajustes en el puesto de trabajo list presenter

--- src/main/java/com/jobits/pos/ui/trabajadores/presenter/PuestoTrabajoListViewPresenter.java
+++ src/main/java/com/jobits/pos/ui/trabajadores/presenter/PuestoTrabajoListViewPresenter.java
@@ -8,7 +8,9 @@ package com.jobits.pos.ui.trabajadores.presenter;
 import com.jobits.pos.controller.trabajadores.PuestoTrabajoDetailController;
 import com.jobits.pos.controller.trabajadores.PuestoTrabajoListController;
 import com.jobits.pos.controller.trabajadores.PuestoTrabajoListService;
+import com.jobits.pos.cordinator.DisplayType;
 import com.jobits.pos.main.Application;
+import com.jobits.pos.notification.TipoNotificacion;
 import com.jobits.pos.ui.presenters.AbstractListViewPresenter;
 import com.jobits.pos.ui.trabajadores.PuestoTrabajoListView;
 
@@ -31,14 +33,27 @@ public class PuestoTrabajoListViewPresenter extends AbstractListViewPresenter<Pu
 
     @Override
     protected void onAgregarClick() {
-        PuestoTrabajoDetailController newController = new PuestoTrabajoDetailController(Application.getInstance().getMainWindow());
+        Application.getInstance().getNavigator().navigateTo("Crear Puesto de Trabajo", null, DisplayType.POPUP);
+
+        //   PuestoTrabajoDetailController newController = new PuestoTrabajoDetailController(Application.getInstance().getMainWindow());
         setListToBean();
 
     }
 
     @Override
     protected void onEditarClick() {
-        PuestoTrabajoDetailController newController = new PuestoTrabajoDetailController(getBean().getElemento_seleccionado(), Application.getInstance().getMainWindow());
+        if (getBean().getElemento_seleccionado() == null) {
+            Application.getInstance().getNotificationService().notify("Debe seleccionar una trabajador", TipoNotificacion.ERROR);
+            return;
+        }
+        Application.getInstance().getNavigator().navigateTo(
+                "Crear Puesto de Trabajo",
+                new PuestoTrabajoDetailViewPresenter(
+                        new PuestoTrabajoDetailController(
+                                getBean().getElemento_seleccionado())),
+                DisplayType.POPUP);
+
+        //  PuestoTrabajoDetailController newController = new PuestoTrabajoDetailController(getBean().getElemento_seleccionado(), Application.getInstance().getMainWindow());
         setListToBean();
     }
 
