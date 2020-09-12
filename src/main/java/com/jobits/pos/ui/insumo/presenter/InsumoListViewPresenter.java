a2bdb0f044caa0673129577a10916c5aa9077b17
Jorge <Jorge@localhost>
2020-09-12 02:43:27 -0400

    Ajustes de navegacion en insumo list Presenter

--- src/main/java/com/jobits/pos/ui/insumo/presenter/InsumoListViewPresenter.java
+++ src/main/java/com/jobits/pos/ui/insumo/presenter/InsumoListViewPresenter.java
@@ -5,11 +5,17 @@
  */
 package com.jobits.pos.ui.insumo.presenter;
 
-import com.jobits.pos.controller.insumo.InsumoCreateEditController;
+import com.jobits.pos.controller.insumo.InsumoDetailController;
 import com.jobits.pos.controller.insumo.InsumoListController;
 import com.jobits.pos.controller.insumo.InsumoListService;
+import com.jobits.pos.controller.productos.ProductoVentaDetailController;
+import com.jobits.pos.cordinator.DisplayType;
+import com.jobits.pos.cordinator.NavigationService;
 import com.jobits.pos.main.Application;
+import com.jobits.pos.ui.insumo.InsumoDetailView;
 import com.jobits.pos.ui.presenters.AbstractListViewPresenter;
+import com.jobits.pos.ui.productos.ProductoVentaDetailView;
+import com.jobits.pos.ui.productos.presenter.ProductoVentaDetailPresenter;
 
 /**
  *
@@ -30,14 +36,21 @@ public class InsumoListViewPresenter extends AbstractListViewPresenter<InsumoLis
 
     @Override
     protected void onAgregarClick() {
-        InsumoCreateEditController newController = new InsumoCreateEditController(Application.getInstance().getMainWindow());
+        NavigationService.getInstance().navigateTo(InsumoDetailView.VIEW_NAME,
+                new InsumoDetailViewPresenter(
+                        new InsumoDetailController()), DisplayType.POPUP);
+//        InsumoDetailController newController = new InsumoDetailController(Application.getInstance().getMainWindow());
         setListToBean();
     }
 
     @Override
     protected void onEditarClick() {
-        InsumoCreateEditController newController = new InsumoCreateEditController(
-                getBean().getElemento_seleccionado(), Application.getInstance().getMainWindow());
+        NavigationService.getInstance().navigateTo(InsumoDetailView.VIEW_NAME,
+                new InsumoDetailViewPresenter(
+                        new InsumoDetailController(
+                                getBean().getElemento_seleccionado())), DisplayType.POPUP);
+//        InsumoDetailController newController = new InsumoDetailController(
+//                getBean().getElemento_seleccionado(), Application.getInstance().getMainWindow());
         setListToBean();
 
     }
