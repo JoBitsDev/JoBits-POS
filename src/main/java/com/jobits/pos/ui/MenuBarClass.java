149a5b1843e7f1310d6d4297b53349ce67147b6b
Jorge <Jorge@localhost>
2020-10-06 11:02:25 -0400

    Creado Menu Bar

---
+++ src/main/java/com/jobits/pos/ui/MenuBarClass.java
@@ -0,0 +1,215 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.ui;
+
+import static com.jobits.pos.ui.MenuBarClassPresenter.*;
+import com.jobits.pos.ui.presenters.AbstractViewPresenter;
+import javax.swing.JMenuBar;
+
+/**
+ *
+ * @author Home
+ */
+public class MenuBarClass extends AbstractViewPanel {
+
+    public static final String VIEW_NAME = "Menu Bar Class";
+
+    private static MenuBarClass menuBar;
+
+    public MenuBarClass(AbstractViewPresenter presenter) {
+        super(presenter);
+    }
+
+    public static MenuBarClass getInstance() {
+        if (menuBar == null) {
+            menuBar = new MenuBarClass(new MenuBarClassPresenter());
+        }
+        return menuBar;
+    }
+
+    public JMenuBar getMainManuBar() {
+        return jMenuBarMainManuBar;
+    }
+
+    // Variables declaration - do not modify                
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItemOcultarBarraEstado;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItemOcultarBarraLateral;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItemSiemprePrimerPlano;
+    private javax.swing.JMenu jMenuAyuda;
+    private javax.swing.JMenuBar jMenuBarMainManuBar;
+    private javax.swing.JMenu jMenuEdicion;
+    private javax.swing.JMenu jMenuHerramientas;
+    private javax.swing.JMenuItem jMenuItemAcercaJobitPOS;
+    private javax.swing.JMenuItem jMenuItemActivarLicencia;
+    private javax.swing.JMenuItem jMenuItemCambiarUsuario;
+    private javax.swing.JMenuItem jMenuItemCerrarSesion;
+    private javax.swing.JMenuItem jMenuItemCopiasSeguridad;
+    private javax.swing.JMenuItem jMenuItemDuplicar;
+    private javax.swing.JMenuItem jMenuItemEliminar;
+    private javax.swing.JMenuItem jMenuItemManualUsuario;
+    private javax.swing.JMenuItem jMenuItemNuevo;
+    private javax.swing.JMenuItem jMenuItemPreferencias;
+    private javax.swing.JMenuItem jMenuItemReportarBug;
+    private javax.swing.JMenuItem jMenuItemSalir;
+    private javax.swing.JMenuItem jMenuItemSeleccionarTodo;
+    private javax.swing.JMenuItem jMenuItemUbicaciones;
+    private javax.swing.JMenu jMenuJoBitsPOS;
+    private javax.swing.JMenu jMenuVista;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    // End of variables declaration    
+
+    @Override
+    public void wireUp() {
+        //JoBits POS
+        jMenuItemAcercaJobitPOS.addActionListener(getPresenter().getOperation(ACTION_SHOW_ACERCA_DE));
+        jMenuItemPreferencias.addActionListener(getPresenter().getOperation(ACTION_SHOW_PREFERENCIAS));
+        jMenuItemReportarBug.addActionListener(getPresenter().getOperation(ACTION_SHOW_REPORTAR_BUG));
+        jMenuItemCambiarUsuario.addActionListener(getPresenter().getOperation(ACTION_SHOW_LOGIN));
+        jMenuItemCerrarSesion.addActionListener(getPresenter().getOperation(ACTION_CERRAR_SESION));
+        jMenuItemSalir.addActionListener(getPresenter().getOperation(ACTION_SALIR));
+        //Edicion
+//        jMenuItemSalir.addActionListener(getPresenter().getOperation(ACTION_SALIR));
+//        jMenuItemSalir.addActionListener(getPresenter().getOperation(ACTION_SALIR));
+//        jMenuItemSalir.addActionListener(getPresenter().getOperation(ACTION_SALIR));
+//        jMenuItemSalir.addActionListener(getPresenter().getOperation(ACTION_SALIR));
+        //Vista
+        jCheckBoxMenuItemOcultarBarraEstado.addActionListener(getPresenter().getOperation(ACTION_OCULTAR_STATUS_BAR));
+        jCheckBoxMenuItemOcultarBarraLateral.addActionListener(getPresenter().getOperation(ACTION_OCULTAR_MENU_LATERAL));
+        jCheckBoxMenuItemSiemprePrimerPlano.addActionListener(getPresenter().getOperation(ACTION_SIEMPRE_PRIMER_PLANO));
+        //Herramientas
+        jMenuItemCopiasSeguridad.addActionListener(getPresenter().getOperation(ACTION_SHOW_COPIAS_SEGURIDAD));
+        jMenuItemUbicaciones.addActionListener(getPresenter().getOperation(ACTION_SHOW_UBICACIONES));
+        jMenuItemActivarLicencia.addActionListener(getPresenter().getOperation(ACTION_SHOW_ACTIVAR_LICENCIA));
+        //Ayuda
+        jMenuItemManualUsuario.addActionListener(getPresenter().getOperation(ACTION_SHOW_MANUAL_USUARIO));
+    }
+
+    @Override
+    public void uiInit() {
+        jMenuBarMainManuBar = new javax.swing.JMenuBar();
+        jMenuJoBitsPOS = new javax.swing.JMenu();
+        jMenuItemAcercaJobitPOS = new javax.swing.JMenuItem();
+        jMenuItemPreferencias = new javax.swing.JMenuItem();
+        jMenuItemReportarBug = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        jMenuItemCerrarSesion = new javax.swing.JMenuItem();
+        jMenuItemCambiarUsuario = new javax.swing.JMenuItem();
+        jSeparator2 = new javax.swing.JPopupMenu.Separator();
+        jMenuItemSalir = new javax.swing.JMenuItem();
+        jMenuEdicion = new javax.swing.JMenu();
+        jMenuItemNuevo = new javax.swing.JMenuItem();
+        jMenuItemDuplicar = new javax.swing.JMenuItem();
+        jMenuItemEliminar = new javax.swing.JMenuItem();
+        jMenuItemSeleccionarTodo = new javax.swing.JMenuItem();
+        jMenuVista = new javax.swing.JMenu();
+        jCheckBoxMenuItemOcultarBarraLateral = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItemOcultarBarraEstado = new javax.swing.JCheckBoxMenuItem();
+        jSeparator3 = new javax.swing.JPopupMenu.Separator();
+        jCheckBoxMenuItemSiemprePrimerPlano = new javax.swing.JCheckBoxMenuItem();
+        jMenuHerramientas = new javax.swing.JMenu();
+        jMenuItemCopiasSeguridad = new javax.swing.JMenuItem();
+        jMenuItemUbicaciones = new javax.swing.JMenuItem();
+        jMenuItemActivarLicencia = new javax.swing.JMenuItem();
+        jMenuAyuda = new javax.swing.JMenu();
+        jMenuItemManualUsuario = new javax.swing.JMenuItem();
+
+        jMenuJoBitsPOS.setBackground(DefaultValues.SECONDARY_COLOR_LIGHT);
+        jMenuEdicion.setBackground(DefaultValues.SECONDARY_COLOR_LIGHT);
+        jMenuVista.setBackground(DefaultValues.SECONDARY_COLOR_LIGHT);
+        jMenuHerramientas.setBackground(DefaultValues.SECONDARY_COLOR_LIGHT);
+        jMenuAyuda.setBackground(DefaultValues.SECONDARY_COLOR_LIGHT);
+        jMenuBarMainManuBar.setBackground(DefaultValues.SECONDARY_COLOR_LIGHT);
+
+        jMenuJoBitsPOS.setText("JoBits POS");
+
+        jMenuItemAcercaJobitPOS.setText("Acerca de JoBits POS");
+
+        jMenuJoBitsPOS.add(jMenuItemAcercaJobitPOS);
+
+        jMenuItemPreferencias.setText("Preferencias");
+        jMenuJoBitsPOS.add(jMenuItemPreferencias);
+
+        jMenuItemReportarBug.setText("Reportar Bug");
+        jMenuJoBitsPOS.add(jMenuItemReportarBug);
+        jMenuJoBitsPOS.add(jSeparator1);
+
+        java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("Strings"); // NOI18N
+        jMenuItemCerrarSesion.setText(bundle.getString("label_cerrar_sesion")); // NOI18N
+        jMenuJoBitsPOS.add(jMenuItemCerrarSesion);
+
+        jMenuItemCambiarUsuario.setText("Cambiar de Usuario");
+        jMenuJoBitsPOS.add(jMenuItemCambiarUsuario);
+        jMenuJoBitsPOS.add(jSeparator2);
+
+        jMenuItemSalir.setText("Salir");
+        jMenuJoBitsPOS.add(jMenuItemSalir);
+
+        jMenuBarMainManuBar.add(jMenuJoBitsPOS);
+
+//        jMenuEdicion.setText(bundle.getString("label_edicion")); // NOI18N
+//
+//        jMenuItemNuevo.setText("Nuevo");
+//        jMenuEdicion.add(jMenuItemNuevo);
+//
+//        jMenuItemDuplicar.setText("Duplicar");
+//        jMenuEdicion.add(jMenuItemDuplicar);
+//
+//        jMenuItemEliminar.setText("Eliminar");
+//        jMenuEdicion.add(jMenuItemEliminar);
+//
+//        jMenuItemSeleccionarTodo.setText("Seleccionar Todo");
+//        jMenuEdicion.add(jMenuItemSeleccionarTodo);
+
+//        jMenuBarMainManuBar.add(jMenuEdicion);
+////        TODO: Agregar Seccion de Edicion en el MenuBar
+
+        jMenuVista.setText("Vista");
+
+        jCheckBoxMenuItemOcultarBarraLateral.setSelected(true);
+        jCheckBoxMenuItemOcultarBarraLateral.setText("Mostrar Barra Lateral");
+        jMenuVista.add(jCheckBoxMenuItemOcultarBarraLateral);
+
+        jCheckBoxMenuItemOcultarBarraEstado.setSelected(true);
+        jCheckBoxMenuItemOcultarBarraEstado.setText("Mostrar Barra de Estado");
+        jMenuVista.add(jCheckBoxMenuItemOcultarBarraEstado);
+        jMenuVista.add(jSeparator3);
+
+        jCheckBoxMenuItemSiemprePrimerPlano.setSelected(false);
+        jCheckBoxMenuItemSiemprePrimerPlano.setText("Siempre en Primer Plano");
+        jMenuVista.add(jCheckBoxMenuItemSiemprePrimerPlano);
+
+        jMenuBarMainManuBar.add(jMenuVista);
+
+        jMenuHerramientas.setText("Herramientas");
+
+        jMenuItemCopiasSeguridad.setText("Copias de Seguridad");
+        jMenuHerramientas.add(jMenuItemCopiasSeguridad);
+
+        jMenuItemUbicaciones.setText("Ubicaciones");
+        jMenuHerramientas.add(jMenuItemUbicaciones);
+
+        jMenuItemActivarLicencia.setText("Activar Licencia");
+        jMenuHerramientas.add(jMenuItemActivarLicencia);
+
+        jMenuBarMainManuBar.add(jMenuHerramientas);
+
+        jMenuAyuda.setText("Ayuda");
+
+        jMenuItemManualUsuario.setText("Manual de Usuario");
+        jMenuAyuda.add(jMenuItemManualUsuario);
+
+        jMenuBarMainManuBar.add(jMenuAyuda);
+
+    }
+
+    @Override
+    public String getViewName() {
+        return VIEW_NAME;
+    }
+
+}
---
+++ src/main/java/com/jobits/pos/ui/MenuBarClassPresenter.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.ui;
+
+import com.jobits.pos.controller.configuracion.ConfiguracionController;
+import com.jobits.pos.controller.configuracion.ConfiguracionService;
+import com.jobits.pos.controller.login.LogInController;
+import com.jobits.pos.controller.login.UbicacionConexionController;
+import com.jobits.pos.controller.reportes.ReportarBugController;
+import com.jobits.pos.controller.reportes.ReportarBugService;
+import com.jobits.pos.cordinator.DisplayType;
+import com.jobits.pos.main.Application;
+import com.jobits.pos.recursos.R;
+import static com.jobits.pos.ui.AcercaDeViewPresenter.ACTION_CLOSE;
+import com.jobits.pos.ui.backup.BackUpView;
+import com.jobits.pos.ui.backup.presenter.BackUpViewPresenter;
+import com.jobits.pos.ui.configuracion.ConfiguracionView;
+import com.jobits.pos.ui.configuracion.presenter.ConfigurationViewPresenter;
+import com.jobits.pos.ui.licencia.LicenceDialogView;
+import com.jobits.pos.ui.login.LogInView;
+import com.jobits.pos.ui.login.UbicacionView;
+import com.jobits.pos.ui.login.presenter.LoginViewPresenter;
+import com.jobits.pos.ui.login.presenter.UbicacionViewPresenter;
+import com.jobits.pos.ui.presenters.AbstractViewAction;
+import com.jobits.pos.ui.presenters.AbstractViewPresenter;
+import com.jobits.pos.ui.reportes.ReportarBugView;
+import com.jobits.pos.ui.reportes.presenter.ReportarBugViewPresenter;
+import java.awt.BorderLayout;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Home
+ */
+public class MenuBarClassPresenter extends AbstractViewPresenter<MenuBarClassViewModel> {
+
+    //JoBits POS
+    public static final String ACTION_SHOW_ACERCA_DE = "Acerca De";
+    public static final String ACTION_SHOW_PREFERENCIAS = "Preferencias";
+    public static final String ACTION_SHOW_REPORTAR_BUG = "Reportar Bug";
+    public static final String ACTION_SALIR = "Salir";
+    public static final String ACTION_SHOW_LOGIN = "Cambiar Usuario";
+    public static final String ACTION_CERRAR_SESION = "Cerrar Sesion";
+    //Vista
+    public static final String ACTION_OCULTAR_STATUS_BAR = "Ocultar Barra de Estado";
+    public static final String ACTION_OCULTAR_MENU_LATERAL = "Ocultar Menu Lateral";
+    public static final String ACTION_SIEMPRE_PRIMER_PLANO = "Siempre Primer Plano";
+    //Herramientas
+    public static final String ACTION_SHOW_COPIAS_SEGURIDAD = "Copias de Seguridad";
+    public static final String ACTION_SHOW_UBICACIONES = "Ubicaciones";
+    public static final String ACTION_SHOW_ACTIVAR_LICENCIA = "Activar Licencia";
+    //Ayuda
+    public static final String ACTION_SHOW_MANUAL_USUARIO = "Manual de Usuario";
+
+    public MenuBarClassPresenter() {
+        super(new MenuBarClassViewModel());
+    }
+
+    @Override
+    protected void registerOperations() {
+
+        registerOperation(new AbstractViewAction(ACTION_SHOW_ACERCA_DE) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateTo(
+                        AcercaDeView.VIEW_NAME, new AcercaDeViewPresenter(), DisplayType.POPUP);
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_SHOW_PREFERENCIAS) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateTo(
+                        ConfiguracionView.VIEW_NAME, new ConfigurationViewPresenter(new ConfiguracionController()), DisplayType.POPUP);
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_SHOW_REPORTAR_BUG) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateTo(
+                        ReportarBugView.VIEW_NAME, new ReportarBugViewPresenter(new ReportarBugController()), DisplayType.POPUP);
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_SHOW_LOGIN) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateTo(
+                        LogInView.VIEW_NAME, new LoginViewPresenter(new LogInController()), DisplayType.POPUP);
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_CERRAR_SESION) {
+            @Override
+            public Optional doAction() {
+                JOptionPane.showMessageDialog(Application.getInstance().getMainWindow(), "En Desarrollo");
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_SALIR) {
+            @Override
+            public Optional doAction() {
+                Object[] options = {"Si", "No"};
+                int confirm = JOptionPane.showOptionDialog(
+                        Application.getInstance().getMainWindow(),
+                        R.RESOURCE_BUNDLE.getString("label_dialogo_salir_sistema"),
+                        null,
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        options,
+                        options[0]);
+
+                if (confirm == JOptionPane.YES_OPTION) {
+                    Application.getInstance().getMainWindow().dispose();
+                }
+                return Optional.empty();
+            }
+        });
+
+        registerOperation(new AbstractViewAction(ACTION_OCULTAR_STATUS_BAR) {
+            @Override
+            public Optional doAction() {
+                boolean a = !RootView.getInstance().isStatusBarVisibility();
+                RootView.getInstance().getjPanelStatus().setVisible(a);
+                RootView.getInstance().setStatusBarVisibility(a);
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_OCULTAR_MENU_LATERAL) {
+            @Override
+            public Optional doAction() {
+                RootView.getInstance().getDashboard().getTaskPane().getComponent(1).setVisible(
+                        !RootView.getInstance().getDashboard().getTaskPane().getComponent(1).isVisible());
+                //TODO: pedir el nombre del componente, no el componente por indice
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_SIEMPRE_PRIMER_PLANO) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getMainWindow().setAlwaysOnTop(
+                        !Application.getInstance().getMainWindow().isAlwaysOnTop());
+                return Optional.empty();
+            }
+        });
+
+        registerOperation(new AbstractViewAction(ACTION_SHOW_COPIAS_SEGURIDAD) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateTo(
+                        BackUpView.VIEW_NAME, new BackUpViewPresenter(new UbicacionConexionController()), DisplayType.POPUP);
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_SHOW_UBICACIONES) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateTo(
+                        UbicacionView.VIEW_NAME, new UbicacionViewPresenter(new UbicacionConexionController()), DisplayType.POPUP);
+                return Optional.empty();
+            }
+        });
+        registerOperation(new AbstractViewAction(ACTION_SHOW_ACTIVAR_LICENCIA) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateTo(
+                        LicenceDialogView.VIEW_NAME, DisplayType.POPUP);
+                return Optional.empty();
+            }
+        });
+
+        registerOperation(new AbstractViewAction(ACTION_SHOW_MANUAL_USUARIO) {
+            @Override
+            public Optional doAction() {
+                JOptionPane.showMessageDialog(Application.getInstance().getMainWindow(), "En Desarrollo");
+                return Optional.empty();
+            }
+        });
+
+    }
+
+}
