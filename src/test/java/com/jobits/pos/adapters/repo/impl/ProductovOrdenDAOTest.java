e3fef4128fcb0db83a8451584785751fb79f8468
Jorge <Jorge@localhost>
2020-11-08 19:00:56 -0500

    test agregado

---
+++ src/test/java/com/jobits/pos/adapters/repo/impl/ProductovOrdenDAOTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.adapters.repo.impl;
+
+import com.jobits.pos.controller.login.UbicacionConexionController;
+import com.jobits.pos.domain.models.ProductovOrden;
+import com.jobits.pos.recursos.DBConnector;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jorge
+ */
+public class ProductovOrdenDAOTest {
+
+    public ProductovOrdenDAOTest() {
+    }
+
+    @Before
+    public void setUp() {
+        DBConnector.init(new UbicacionConexionController().getUbicaciones().getUbicacionActiva());
+    }
+
+    /**
+     * Test of getInstance method, of class ProductovOrdenDAO.
+     */
+    @Test
+    public void testGetInstance() {
+        ProductovOrdenDAO.getInstance().startTransaction();
+        for (ProductovOrden pvo : ProductovOrdenDAO.getInstance().findAll()) {
+            pvo.setPrecioVendido(pvo.getProductoVenta().getPrecioVenta());
+           // pvo.setNombreProductoVendido(pvo.getProductoVenta().getNombre());
+            ProductovOrdenDAO.getInstance().edit(pvo);
+        }
+        ProductovOrdenDAO.getInstance().commitTransaction();
+    }
+
+}
