1ea6f848cd8671f7e6f2cc29f7eaab4982948416
Jorge <Jorge@localhost>
2020-11-08 19:39:51 -0500

    Clientes List View Presenter Model Creados

---
+++ src/main/java/com/jobits/pos/ui/clientes/presenter/ClientesListViewModel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jobits.pos.ui.clientes.presenter;
+
+import com.jobits.pos.ui.productos.presenter.*;
+import com.jobits.pos.domain.models.ProductoVenta;
+import com.jobits.pos.ui.viewmodel.AbstractListViewModel;
+
+/**
+ * 
+ * JoBits
+ * @author Home
+ * 
+ */
+public class ClientesListViewModel extends AbstractListViewModel<ProductoVenta> {
+
+}
