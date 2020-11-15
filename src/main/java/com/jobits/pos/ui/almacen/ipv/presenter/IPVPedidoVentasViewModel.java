b16cee654e1e1af51673562da51caa07c518022c
Jorge <Jorge@localhost>
2020-11-15 18:59:23 -0500

    IPVPedidoVentas View Presenter Model

---
+++ src/main/java/com/jobits/pos/ui/almacen/ipv/presenter/IPVPedidoVentasViewModel.java
@@ -0,0 +1,220 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.ui.almacen.ipv.presenter;
+
+import com.jgoodies.common.collect.ArrayListModel;
+import com.jobits.pos.domain.InsumoPedidoModel;
+import com.jobits.pos.domain.ProdcutoVentaPedidoModel;
+import com.jobits.pos.domain.models.Almacen;
+import com.jobits.pos.domain.models.IpvVentaRegistro;
+import com.jobits.pos.ui.viewmodel.AbstractViewModel;
+
+/**
+ *
+ * @author Home
+ */
+public class IPVPedidoVentasViewModel extends AbstractViewModel {
+
+    //
+    //LISTAS
+    //
+    private ArrayListModel<IpvVentaRegistro> lista_ipv_venta_registro = new ArrayListModel<>();
+
+    public static final String PROP_LISTA_IPV_VENTA_REGISTRO = "lista_ipv_venta_registro";
+
+    private ArrayListModel<ProdcutoVentaPedidoModel> lista_producto_venta_model = new ArrayListModel<>();
+
+    public static final String PROP_LISTA_PRODUCTO_VENTA_MODEL = "lista_producto_venta_model";
+
+    private ArrayListModel<Almacen> lista_almacen = new ArrayListModel<>();
+
+    public static final String PROP_LISTA_ALMACEN = "lista_almacen";
+
+    private ArrayListModel<InsumoPedidoModel> lista_insumo_pedido_model = new ArrayListModel<>();
+
+    public static final String PROP_LISTA_INSUMO_PEDIDO_MODEL = "lista_insumo_pedido_model";
+
+    //
+    //SELECIONADO
+    //
+    private IpvVentaRegistro seleccionado_ipv_ventas;
+
+    public static final String PROP_SELECCIONADO_IPV_VENTAS = "seleccionado_ipv_ventas";
+
+    private ProdcutoVentaPedidoModel seleccionado_producto_venta;
+
+    public static final String PROP_SELECCIONADO_PRODUCTO_VENTA = "seleccionado_producto_venta";
+
+    private Almacen seleccionado_almacen;
+
+    public static final String PROP_SELECCIONADO_ALMACEN = "seleccionado_almacen";
+
+    private InsumoPedidoModel seleccionado_insumo_pedido;
+
+    public static final String PROP_SELECCIONADO_INSUMO_PEDIDO = "seleccionado_insumo_pedido";
+
+    /**
+     * Get the value of seleccionado_insumo_pedido
+     *
+     * @return the value of seleccionado_insumo_pedido
+     */
+    public InsumoPedidoModel getSeleccionado_insumo_pedido() {
+        return seleccionado_insumo_pedido;
+    }
+
+    /**
+     * Set the value of seleccionado_insumo_pedido
+     *
+     * @param seleccionado_insumo_pedido new value of seleccionado_insumo_pedido
+     */
+    public void setSeleccionado_insumo_pedido(InsumoPedidoModel seleccionado_insumo_pedido) {
+        InsumoPedidoModel oldSeleccionado_insumo_pedido = this.seleccionado_insumo_pedido;
+        this.seleccionado_insumo_pedido = seleccionado_insumo_pedido;
+        firePropertyChange(PROP_SELECCIONADO_INSUMO_PEDIDO, oldSeleccionado_insumo_pedido, seleccionado_insumo_pedido);
+    }
+
+    /**
+     * Get the value of seleccionado_almacen
+     *
+     * @return the value of seleccionado_almacen
+     */
+    public Almacen getSeleccionado_almacen() {
+        return seleccionado_almacen;
+    }
+
+    /**
+     * Set the value of seleccionado_almacen
+     *
+     * @param seleccionado_almacen new value of seleccionado_almacen
+     */
+    public void setSeleccionado_almacen(Almacen seleccionado_almacen) {
+        Almacen oldSeleccionado_almacen = this.seleccionado_almacen;
+        this.seleccionado_almacen = seleccionado_almacen;
+        firePropertyChange(PROP_SELECCIONADO_ALMACEN, oldSeleccionado_almacen, seleccionado_almacen);
+    }
+
+    /**
+     * Get the value of seleccionado_producto_venta
+     *
+     * @return the value of seleccionado_producto_venta
+     */
+    public ProdcutoVentaPedidoModel getSeleccionado_producto_venta() {
+        return seleccionado_producto_venta;
+    }
+
+    /**
+     * Set the value of seleccionado_producto_venta
+     *
+     * @param seleccionado_producto_venta new value of
+     * seleccionado_producto_venta
+     */
+    public void setSeleccionado_producto_venta(ProdcutoVentaPedidoModel seleccionado_producto_venta) {
+        ProdcutoVentaPedidoModel oldSeleccionado_producto_venta = this.seleccionado_producto_venta;
+        this.seleccionado_producto_venta = seleccionado_producto_venta;
+        firePropertyChange(PROP_SELECCIONADO_PRODUCTO_VENTA, oldSeleccionado_producto_venta, seleccionado_producto_venta);
+    }
+
+    /**
+     * Get the value of seleccionado_ipv_ventas
+     *
+     * @return the value of seleccionado_ipv_ventas
+     */
+    public IpvVentaRegistro getSeleccionado_ipv_ventas() {
+        return seleccionado_ipv_ventas;
+    }
+
+    /**
+     * Set the value of seleccionado_ipv_ventas
+     *
+     * @param seleccionado_ipv_ventas new value of seleccionado_ipv_ventas
+     */
+    public void setSeleccionado_ipv_ventas(IpvVentaRegistro seleccionado_ipv_ventas) {
+        IpvVentaRegistro oldSeleccionado_ipv_ventas = this.seleccionado_ipv_ventas;
+        this.seleccionado_ipv_ventas = seleccionado_ipv_ventas;
+        firePropertyChange(PROP_SELECCIONADO_IPV_VENTAS, oldSeleccionado_ipv_ventas, seleccionado_ipv_ventas);
+    }
+
+    /**
+     * Get the value of lista_insumo_pedido_model
+     *
+     * @return the value of lista_insumo_pedido_model
+     */
+    public ArrayListModel<InsumoPedidoModel> getLista_insumo_pedido_model() {
+        return lista_insumo_pedido_model;
+    }
+
+    /**
+     * Set the value of lista_insumo_pedido_model
+     *
+     * @param lista_insumo_pedido_model new value of lista_insumo_pedido_model
+     */
+    public void setLista_insumo_pedido_model(ArrayListModel<InsumoPedidoModel> lista_insumo_pedido_model) {
+        ArrayListModel<InsumoPedidoModel> oldLista_insumo_pedido_model = this.lista_insumo_pedido_model;
+        this.lista_insumo_pedido_model = lista_insumo_pedido_model;
+        firePropertyChange(PROP_LISTA_INSUMO_PEDIDO_MODEL, oldLista_insumo_pedido_model, lista_insumo_pedido_model);
+    }
+
+    /**
+     * Get the value of lista_almacen
+     *
+     * @return the value of lista_almacen
+     */
+    public ArrayListModel<Almacen> getLista_almacen() {
+        return lista_almacen;
+    }
+
+    /**
+     * Set the value of lista_almacen
+     *
+     * @param lista_almacen new value of lista_almacen
+     */
+    public void setLista_almacen(ArrayListModel<Almacen> lista_almacen) {
+        ArrayListModel<Almacen> oldLista_almacen = this.lista_almacen;
+        this.lista_almacen = lista_almacen;
+        firePropertyChange(PROP_LISTA_ALMACEN, oldLista_almacen, lista_almacen);
+    }
+
+    /**
+     * Get the value of lista_producto_venta_model
+     *
+     * @return the value of lista_producto_venta_model
+     */
+    public ArrayListModel<ProdcutoVentaPedidoModel> getLista_producto_venta_model() {
+        return lista_producto_venta_model;
+    }
+
+    /**
+     * Set the value of lista_producto_venta_model
+     *
+     * @param lista_producto_venta_model new value of lista_producto_venta_model
+     */
+    public void setLista_producto_venta_model(ArrayListModel<ProdcutoVentaPedidoModel> lista_producto_venta_model) {
+        ArrayListModel<ProdcutoVentaPedidoModel> oldLista_producto_venta_model = this.lista_producto_venta_model;
+        this.lista_producto_venta_model = lista_producto_venta_model;
+        firePropertyChange(PROP_LISTA_PRODUCTO_VENTA_MODEL, oldLista_producto_venta_model, lista_producto_venta_model);
+    }
+
+    /**
+     * Get the value of lista_ipv_venta_registro
+     *
+     * @return the value of lista_ipv_venta_registro
+     */
+    public ArrayListModel<IpvVentaRegistro> getLista_ipv_venta_registro() {
+        return lista_ipv_venta_registro;
+    }
+
+    /**
+     * Set the value of lista_ipv_venta_registro
+     *
+     * @param lista_ipv_venta_registro new value of lista_ipv_venta_registro
+     */
+    public void setLista_ipv_venta_registro(ArrayListModel<IpvVentaRegistro> lista_ipv_venta_registro) {
+        ArrayListModel<IpvVentaRegistro> oldLista_ipv_venta_registro = this.lista_ipv_venta_registro;
+        this.lista_ipv_venta_registro = lista_ipv_venta_registro;
+        firePropertyChange(PROP_LISTA_IPV_VENTA_REGISTRO, oldLista_ipv_venta_registro, lista_ipv_venta_registro);
+    }
+
+}
---
+++ src/main/java/com/jobits/pos/ui/almacen/ipv/presenter/IPVPedidoVentasViewPresenter.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.ui.almacen.ipv.presenter;
+
+import com.jgoodies.common.collect.ArrayListModel;
+import com.jobits.pos.controller.almacen.PedidoIpvVentasController;
+import com.jobits.pos.controller.almacen.PedidoIpvVentasService;
+import com.jobits.pos.domain.InsumoPedidoModel;
+import com.jobits.pos.domain.ProdcutoVentaPedidoModel;
+import com.jobits.pos.domain.VentaDAO1;
+import com.jobits.pos.domain.models.Almacen;
+import com.jobits.pos.domain.models.IpvVentaRegistro;
+import com.jobits.pos.domain.models.ProductoInsumo;
+import com.jobits.pos.main.Application;
+import com.jobits.pos.ui.presenters.AbstractViewAction;
+import com.jobits.pos.ui.presenters.AbstractViewPresenter;
+import com.jobits.pos.ui.utils.NumberPad;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Home
+ */
+public class IPVPedidoVentasViewPresenter extends AbstractViewPresenter<IPVPedidoVentasViewModel> {
+
+    public static String ACTION_CANCELAR = "Cancelar";
+    public static String ACTION_ACEPTAR = "Aceptar";
+
+    public static String ACTION_ELIMINAR_IPV = "Eliminar IPV";
+    public static String ACTION_AGREGAR_IPV = "Agregr IPV";
+
+    private PedidoIpvVentasService service;
+
+    public IPVPedidoVentasViewPresenter(PedidoIpvVentasService controller) {
+        super(new IPVPedidoVentasViewModel());
+        this.service = controller;
+        fillBeanData();
+    }
+
+    @Override
+    protected void registerOperations() {
+        registerOperation(new AbstractViewAction(ACTION_AGREGAR_IPV) {
+            @Override
+            public Optional doAction() {
+                onAgregarIPVClick();
+                actualizarTablaPedido();
+                return Optional.empty();
+            }
+        }
+        );
+        registerOperation(new AbstractViewAction(ACTION_ELIMINAR_IPV) {
+            @Override
+            public Optional doAction() {
+                onEliminarIPVClick();
+                actualizarTablaPedido();
+                return Optional.empty();
+            }
+
+        }
+        );
+        registerOperation(new AbstractViewAction(ACTION_CANCELAR) {
+            @Override
+            public Optional doAction() {
+                Application.getInstance().getNavigator().navigateUp();
+                return Optional.empty();
+            }
+        }
+        );
+        registerOperation(new AbstractViewAction(ACTION_ACEPTAR) {
+            @Override
+            public Optional doAction() {
+                onAceptarClick();
+                return Optional.empty();
+            }
+
+        }
+        );
+    }
+
+    private void onAceptarClick() {
+        if (service.realizarPedidoDeIpv(
+                getBean().getLista_insumo_pedido_model(),
+                getBean().getLista_producto_venta_model(),
+                service.getElaboracion(),
+                getBean().getSeleccionado_almacen())) {
+            Application.getInstance().getNavigator().navigateUp();
+        }
+    }
+
+    private void onAgregarIPVClick() {
+        IpvVentaRegistro selected = getBean().getSeleccionado_ipv_ventas();
+        if (selected != null) {
+            float cantidad;
+            String value = new NumberPad(null).showView();
+            if (!value.equals("") && !value.equals(".")) {
+                cantidad = Float.parseFloat(value);
+                if (cantidad != 0f) {
+                    getBean().getLista_producto_venta_model().add(
+                            new ProdcutoVentaPedidoModel(selected, cantidad));
+                } else {
+                    JOptionPane.showMessageDialog(null, "El valor introducido no es una cantidad valida");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "El valor introducido no es una cantidad valida");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Seleccione un Producto de IPV primero");
+        }
+    }
+
+    private void onEliminarIPVClick() {
+        ProdcutoVentaPedidoModel selected = getBean().getSeleccionado_producto_venta();
+        if (selected != null) {
+            getBean().getLista_producto_venta_model().remove(selected);
+        } else {
+            JOptionPane.showMessageDialog(null, "Seleccione un Producto de IPV primero");
+        }
+    }
+
+    private void fillBeanData() {
+        getBean().setLista_ipv_venta_registro(new ArrayListModel<>(service.getIpvProductList()));
+        getBean().setLista_almacen(new ArrayListModel<>(service.getAlmacenList()));
+        getBean().setSeleccionado_almacen(getBean().getLista_almacen().get(0));
+    }
+
+    private void actualizarTablaPedido() {
+        List<ProdcutoVentaPedidoModel> porPedir = getBean().getLista_producto_venta_model();
+        ArrayList<ProductoInsumo> productoInsumo = new ArrayList<>();
+        for (ProdcutoVentaPedidoModel x : porPedir) {
+            VentaDAO1.joinListsProductoInsumos(productoInsumo, new ArrayList<>(x.getIpvProducto().getProductoVenta().getProductoInsumoList()), x.getCantidad());
+        }
+        List<InsumoPedidoModel> pedido = new ArrayList<>();
+        for (ProductoInsumo x : productoInsumo) {
+            pedido.add(new InsumoPedidoModel(x.getInsumo(), x.getCantidad()));
+        }
+        getBean().getLista_insumo_pedido_model().clear();
+        getBean().getLista_insumo_pedido_model().addAll(pedido);
+    }
+
+}
