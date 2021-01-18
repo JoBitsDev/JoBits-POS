0b1e11ea67122b1d6c42930fc79c03f062b31bce
Jorge <Jorge@localhost>
2021-01-18 17:43:15 -0400

    Vista Ubicaciones LIst

---
+++ src/main/java/com/jobits/pos/ui/reserva/ubicaciones/presenter/UbicacionesListViewModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.ui.reserva.ubicaciones.presenter;
+
+import com.jobits.pos.reserva.core.domain.Ubicacion;
+import com.jobits.pos.ui.viewmodel.AbstractListViewModel;
+
+/**
+ *
+ * @author Home
+ */
+public class UbicacionesListViewModel extends AbstractListViewModel<Ubicacion>{
+    
+}
