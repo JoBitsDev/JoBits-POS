d9e23d6b42602fd08b8f1f84888fdd734cd6e330
Jorge <Jorge@localhost>
2021-01-17 22:10:13 -0400

    Cmbiando parametros al time cell render

--- src/main/java/com/jobits/pos/ui/reserva/TimeCellRender.java
+++ src/main/java/com/jobits/pos/ui/reserva/TimeCellRender.java
@@ -17,12 +17,21 @@ import javax.swing.JList;
  */
 public class TimeCellRender extends DefaultListCellRenderer {
 
+    private String pattern = "h:mm a";
+
+    public TimeCellRender() {
+    }
+
+    public TimeCellRender(String pattern) {
+        this.pattern = pattern;
+    }
+
     @Override
     public Component getListCellRendererComponent(JList list, Object value,
             int index, boolean isSelected, boolean cellHasFocus) {
         super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
-        setText(((LocalTime) value).format(DateTimeFormatter.ofPattern("h:mm a")));
+        setText(((LocalTime) value).format(DateTimeFormatter.ofPattern(pattern)));
 
         return this;
     }
