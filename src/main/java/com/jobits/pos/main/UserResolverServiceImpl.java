797864796f35ccbd304b993760a7254dfc3541c4
Jorge <Jorge@localhost>
2021-01-23 17:34:47 -0500

    User Resolver

--- src/main/java/com/jobits/pos/main/UserResolverServiceImpl.java
+++ src/main/java/com/jobits/pos/main/UserResolverServiceImpl.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.jobits.pos.main;
 
 import com.jobits.pos.core.domain.models.Personal;
@@ -11,15 +10,16 @@ import com.jobits.pos.recursos.R;
 import com.root101.clean.core.app.services.UserResolverService;
 
 /**
- * 
+ *
  * JoBits
+ *
  * @author Jorge
- * 
+ *
  */
-public class UserResolverServiceImpl implements UserResolverService<Personal>{
+public class UserResolverServiceImpl implements UserResolverService<Personal> {
 
     @Override
-    public Personal resolveUser() throws Exception {
+    public Personal resolveUser() {
         return R.loggedUser;
     }
 
