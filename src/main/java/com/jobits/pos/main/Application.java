adcf86a18ab21133fb9708896daf5a244593bc44
Jorge <Jorge@localhost>
2021-05-27 23:10:24 -0400

    MiniH-Incremento de version

--- src/main/java/com/jobits/pos/main/Application.java
+++ src/main/java/com/jobits/pos/main/Application.java
@@ -76,7 +76,7 @@ public class Application {
 
     public static final int MINOR_VERSION = 6;
 
-    public static final int PATCH_VERSION = 0;
+    public static final int PATCH_VERSION = 1;
 
     public static String RELEASE_VERSION = "Version " + MAJOR_VERSION + "." + MINOR_VERSION + "." + PATCH_VERSION;
 
